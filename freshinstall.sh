e2c03a7fa62228d8423e392edf77f01d7fef8f9f
Todd Griffin <nemagnusfrater@gmail.com>
2017-09-29 17:19:01 -0400

    Create freshinstall script to bring your newly installed OS up to working speed quickly.

---
+++ freshinstall.sh
@@ -0,0 +1,14 @@
+# Gdebi (install .deb executable files)
+sudo apt install gdebi
+
+# Vim
+sudo apt install vim
+
+# Git
+sudo apt install git
+
+# Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
