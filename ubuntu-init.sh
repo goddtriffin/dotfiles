ef92ea56c3a64412bbb5f18b00cbb95c2a87bc26
Todd Everett Griffin <goddtriffin@gmail.com>
2020-07-26 23:45:35 -0500

    Add snapcraft installations.

--- ubuntu-init.sh
+++ ubuntu-init.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+# apt
 sudo apt install wget curl vim git zsh snapd
+
+# snapcraft
+sudo snap install android-studio
+sudo snap install audacity
+sudo snap install bitwarden
+sudo snap install blender
+sudo snap install chromium
+sudo snap install code
+sudo snap install ffmpeg
+sudo snap install gimp
+sudo snap install go
+sudo snap install godot --classic
+sudo snap install goland
+sudo snap install intellij-idea-ultimate
+sudo snap install musescore
+sudo snap install slack
+sudo snap install spotify
 
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
